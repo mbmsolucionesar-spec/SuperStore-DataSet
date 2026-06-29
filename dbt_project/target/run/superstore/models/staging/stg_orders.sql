1b0532091a9d0997703f157dffb11e1909f45b8f
Miguel Chura <m.chura3@alumnos.santotomas.cl>
2026-06-29 17:20:13 -0400

    feat: agregar dashboard SuperStore

---
+++ dbt_project/target/run/superstore/models/staging/stg_orders.sql
@@ -0,0 +1,29 @@
+
+  
+  create view "superstore"."analytics"."stg_orders__dbt_tmp" as (
+    
+
+SELECT
+    "Row ID" AS row_id,
+    "Order ID" AS order_id,
+    STRPTIME("Order Date", '%m/%d/%Y') AS order_date,
+    STRPTIME("Ship Date", '%m/%d/%Y') AS ship_date,
+    "Ship Mode" AS ship_mode,
+    "Customer ID" AS customer_id,
+    "Customer Name" AS customer_name,
+    "Segment" AS segment,
+    "Country/Region" AS country_region,
+    "City" AS city,
+    "State/Province" AS state_province,
+    "Postal Code" AS postal_code,
+    "Region" AS region,
+    "Product ID" AS product_id,
+    "Category" AS category,
+    "Sub-Category" AS sub_category,
+    "Product Name" AS product_name,
+    CAST("Sales" AS DOUBLE) AS sales,
+    CAST("Quantity" AS INTEGER) AS quantity,
+    CAST("Discount" AS DOUBLE) AS discount,
+    CAST("Profit" AS DOUBLE) AS profit
+FROM raw_superstore
+  );
